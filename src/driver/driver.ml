594ac511305e494f5be0ee2caaf9b5acbbe5530b
NekrodNIK <60639354+NekrodNIK@users.noreply.github.com>
2026-09-20 16:33:44 +0700

    driver fix

--- src/driver/driver.ml
+++ src/driver/driver.ml
@@ -1,9 +1,14 @@
 open Cmdliner
 open Cmdliner.Term.Syntax
 
+let grammar_ver = 1
+
 let splc src_path lr =
-  let lex = Spl.Lexer.from_string @@ In_channel.with_open_text src_path In_channel.input_all in
-  
+  let lex =
+    Spl.Lexer.from_string
+    @@ In_channel.with_open_text src_path In_channel.input_all
+  in
+
   match lr with
   | Some lr_path ->
       Out_channel.with_open_text lr_path
@@ -14,7 +19,13 @@ let cmd =
   Cmd.v (Cmd.info "splc")
   @@
   let+ src_path = Arg.(required & pos 0 (some file) None & info [] ~docv:"FILE")
-  and+ lr = Arg.(value & opt (some path) None & info [ "t" ]) in
+  and+ lr = Arg.(value & opt (some path) None & info [ "t" ])
+  and+ _ =
+    Arg.(
+      value
+      & opt (enum [ ([%string "%{grammar_ver#Int}"], grammar_ver) ]) grammar_ver
+      & info [ "g" ])
+  in
   splc src_path lr
 
 let () = exit (Cmd.eval cmd)
